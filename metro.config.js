6e7748da9e021035912a6aacbe05d9014f16ac12
Aurelien Essome <aubessome98@gmail.com>
2022-04-21 11:02:32 -0400

    Added new files for EAS build.

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,27 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// expo v43: 
+// remove the @ (see: https://blog.expo.io/expo-sdk-41-12cc5232f2ef)
+const { getDefaultConfig } = require("@expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push('db');
+
+defaultConfig.transformer.minifierConfig.compress.drop_console = true;
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
+
+// module.exports = defaultConfig;
+// module.exports = getDefaultConfig(__dirname);
---
+++ react-native.config.js
@@ -0,0 +1,13 @@
+// File created by expo-dev-client/app.plugin.js
+
+module.exports = {
+    dependencies: {
+      ...require('expo-dev-client/dependencies'),
+    },
+    project: {
+      ios: {},
+      android: {},
+    },
+    assets: ['./assets/fonts']
+  };
+  
